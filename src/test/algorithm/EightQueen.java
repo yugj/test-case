ce65bfbddc2a022ff854351c3f694c1529a9070c
yugj <yugj@hotmail.com>
2014-12-15 18:20:30 +0800

    八皇后

---
+++ src/test/algorithm/EightQueen.java
@@ -0,0 +1,27 @@
+package test.algorithm;
+
+/**
+ * @author yugj
+ * @date 2014年12月15日 下午12:54:28
+ */
+public class EightQueen {
+	//save position of queen
+	private int[] queen;
+	
+	public EightQueen () { 
+		for(int i=0; i<8; i++) { 
+			queen[i] = 0;
+		}
+	}
+	
+	private void execute(int i) { 
+		
+	}
+	
+	private boolean checkPos(int i) { 
+		return false;
+	}
+	
+	public static void main(String[] args) {
+	}
+}
---
+++ src/test/exception/TestTryCatchFinally.java
@@ -0,0 +1,5 @@
+package test.exception;
+
+public class TestTryCatchFinally {
+	
+}
