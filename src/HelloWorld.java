f7baf461fb96a8730a78615b01251c75917b3581
yugj <yugj@hotmail.com>
2014-12-16 15:07:33 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+import java.awt.Graphics;
+import javax.swing.JApplet;
+public class HelloWorld extends JApplet{
+ public void paint(Graphics g){
+  g.drawString("HELLO WORLD",25,25);
+ }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,6 @@
 
 public class Test {
 	public static void main(String[] args) {
-		String bir = "1234567890123456";
-		String f8 = bir.substring(bir.length()-16, bir.length() - 8);
-		System.out.println(f8);
+		System.out.println("hello");
 	}
 }
