90f926371ae564e7592747a455c670822c679343
yugj <yugj@hotmail.com>
2015-10-17 19:36:24 +0800

    test

--- src/test/TestByte.java
+++ src/test/TestByte.java
@@ -1,17 +1,17 @@
 package test;
 
 /**
- * 如何测试字符占的比特位
+ * 
  *  
  * @author ygj
  *
  */
 public class TestByte {
 	public static void main(String[] args) {
-		char a = '余';
+		char a = 'a';
 		System.out.println(a);
 		
-		System.out.println("余".length());
+		System.out.println("".length());
 		
 		
 		
