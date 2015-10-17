adf0c897327361c91a2f6c69a47668e2a3d23a57
yugj <yugj@hotmail.com>
2015-10-17 19:39:45 +0800

    test

--- src/AppletHelloWorld.java
+++ src/AppletHelloWorld.java
@@ -1,6 +1,6 @@
 import java.awt.Graphics;
 import javax.swing.JApplet;
-public class HelloWorld extends JApplet{
+public class AppletHelloWorld extends JApplet{
  public void paint(Graphics g){
   g.drawString("HELLO WORLD",25,25);
  }
