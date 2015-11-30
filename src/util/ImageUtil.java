dd4b84cd336e36abd87c6d2be454b90fcb632563
yugj <yugj@hotmail.com>
2015-11-30 17:04:30 +0800

    fix

--- src/util/ImageUtil.java
+++ src/util/ImageUtil.java
@@ -12,9 +12,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
- 
+
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
  
 /**
  * 图片水印
@@ -26,81 +25,11 @@ public class ImageUtil {
      */
     public static void main(String[] args) {
         String srcImgPath = "f:/temp/1.png";
-        String iconPath = "f:/temp/2.jpg";
         String targerPath = "f:/temp/4.png";
-        // 给图片添加水印
-//      ImageUtil.waterMarkImageByIcon("java水印图片", iconPath, srcImgPath, targerPath, 0, 0, 0, 0.5f);
-        ImageUtil.waterMarkByText("java水印图片", srcImgPath, targerPath, 45, 100, 100, 0.5f);
         // 给图片添加水印,水印旋转-45
-        //       
+        ImageUtil.waterMarkByText("java水印图片", srcImgPath, targerPath, 45, 100, 100, 0.5f);
     }
  
-    /**
-     * 给图片添加水印、可设置水印图片旋转角度
-     * 
-     * @param iconPath
-     *            水印图片路径
-     * @param srcImgPath
-     *            源图片路径
-     * @param targerPath
-     *            目标图片路径
-     * @param degree
-     *            水印图片旋转角度
-     * @param width
-     *            宽度(与左相比)
-     * @param height
-     *            高度(与顶相比)
-     * @param clarity
-     *            透明度(小于1的数)越接近0越透明
-     */
-    public static void waterMarkImageByIcon(String logoText, String iconPath, String srcImgPath, String targerPath,
-            Integer degree, Integer width, Integer height, float clarity) {
-        OutputStream os = null;
-        try {
-            Image srcImg = ImageIO.read(new File(srcImgPath));
-            System.out.println("width:" + srcImg.getWidth(null));
-            System.out.println("height:" + srcImg.getHeight(null));
-            BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null), srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
-            // 得到画笔对象
-            Graphics2D g = buffImg.createGraphics();
-            // 设置对线段的锯齿状边缘处理
-            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null);
-            if (null != degree) {
-                // 设置水印旋转
-                g.rotate(Math.toRadians(degree), (double) buffImg.getWidth() / 2, (double) buffImg.getHeight() / 2);
-            }
-            // 水印图象的路径 水印一般为gif或者png的，这样可设置透明度
-            ImageIcon imgIcon = new ImageIcon(iconPath);
-            // 得到Image对象。
-            Image img = imgIcon.getImage();
-            float alpha = clarity; // 透明度
-            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
-            // 表示水印图片的位置
-            //          g.drawImage(img, width, height, null);
-            g.drawImage(img, srcImg.getWidth(null) - img.getWidth(null), srcImg.getHeight(null) - img.getHeight(null), null);
- 
-            //          g.drawString(logoText, srcImg.getWidth(null) - img.getWidth(null), srcImg.getHeight(null) - img.getHeight(null));
- 
-            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
-            g.dispose();
-            os = new FileOutputStream(targerPath);
-            // 生成图片
-            ImageIO.write(buffImg, "JPG", os);
-            System.out.println("添加水印图片完成!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (null != os) { 
-                	os.close();
-                	os = null;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
  
     /**
      * 给图片添加水印、可设置水印图片旋转角度
@@ -129,19 +58,14 @@ public class ImageUtil {
             Image srcImg = ImageIO.read(new File(srcImgPath));
             BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null), srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
             // 得到画笔对象
-            // Graphics g= buffImg.getGraphics();
             Graphics2D g = buffImg.createGraphics();
             // 设置对线段的锯齿状边缘处理
             g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
             g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null);
-            if (null != degree) {
-                // 设置水印旋转
-                g.rotate(Math.toRadians(degree), (double) buffImg.getWidth() / 2, (double) buffImg.getHeight() / 2);
-            }
-            // 设置颜色
+            g.rotate(Math.toRadians(degree), (double) buffImg.getWidth() / 2, (double) buffImg.getHeight() / 2);
             g.setColor(Color.red);
-            // 设置 Font
             g.setFont(new Font("宋体", Font.BOLD, 30));
+            
             float alpha = clarity;
             g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
             // 第一参数->设置的内容，后面两个参数->文字在图片上的坐标位置(x,y) .
@@ -155,14 +79,14 @@ public class ImageUtil {
             e.printStackTrace();
         } finally {
             try {
-                if (null != is)
-                    is.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                if (null != os)
-                    os.close();
+                if (null != is) { 
+                	is.close();
+                	is = null;
+                }
+                if(null != os) {
+                	os.close();
+                	os = null;
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
